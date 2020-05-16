c86beb127488c22904a41ee87b9e4503206f027f
Patick Ongeche <patrick.ongeche@mawingunetworks.com>
2020-05-16 15:13:31 +0300

    causes

--- src/components/Cuases.jsx
+++ src/components/Cuases.jsx
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react'
+import Gallery from 'react-grid-gallery';
+
+const IMAGES =
+[{
+        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        isSelected: true,
+        caption: "After Rain (Jeshu John - designerspics.com)"
+}]
 
 class Causes extends Component {
     state = {  }
     render() { 
         return ( 
             <React.Fragment>
-                this is causes
+                 <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+      <Gallery images={IMAGES}/>
+      <br/>
+      <br/>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+     <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
             </React.Fragment>
          );
     }
